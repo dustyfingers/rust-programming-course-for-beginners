4cfb9571c9289d3ce0f81c8d5908232058efd6d9
lou <dustyfingers01257@gmail.com>
2022-06-09 17:48:14 -0600

    started work on image combiner project

--- combiner/src/main.rs
+++ combiner/src/main.rs
@@ -1,7 +1,36 @@
 mod args;
 use args::Args;
 
-fn main() {
+use image::{ io::Reader, DynamicImage, ImageFormat };
+use std::{ io::BufReader, fs::File };
+
+enum ImageDataErrors {
+    DifferentImageFormats,
+}
+
+fn main() -> Result<(), ImageDataErrors> {
     let args = Args::new();
-    println!("{:?}", args);
+    let (image_1, image_format_1) = find_image_from_path(args.image_1);
+    let (image_2, image_format_2) = find_image_from_path(args.image_2);
+
+    if image_format_1 != image_format_2 {
+        Err(ImageDataErrors.DifferentImageFormats)
+    } else {
+        Ok(())
+    }
+}
+
+
+fn find_image_from_path(path: String) -> (DynamicImage, ImageFormat) {
+    let image_reader: Reader<BufReader<File>> = Reader::open(path).unwrap();
+    let image_format: ImageFormat = image_reader.format().unwrap();
+    let image: DynamicImage = image_reader.decode().unwrap();
+    (image, image_format)
+}
+
+fn get_smallest_dimension(dim_1: (u32, u32), dim_2: (u32, u32)) -> (u32, u32) {
+    let pix_num_1 = dim_1.0 * dim_1.1;
+    let pix_num_2 = dim_2.0 * dim_2.1;
+
+    return if pix_num_1 < pix_num_2 { dim_1 } else { dim_2 };
 }
